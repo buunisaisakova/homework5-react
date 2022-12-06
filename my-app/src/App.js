2e6f5e6bd2b4b8ef96722b230f0fff7e1ac58d6c
buunisaisakova <isannisan97@gmail.com>
2022-12-06 11:10:24 +0600

    kk

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ContactsPage from "./pages/contaktsPage/ContactsPage";
+import AboutUs from "./pages/aboutUs/AboutUs";
+import PortfolioPage from "./pages/portfolioPage/PortfolioPage";
+
+function App() {
+  return (
+    <div className="App">
+
+      <AboutUs/>
+      <PortfolioPage/>
+      <ContactsPage/>
+          </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/pages/portfolioPage/portfolioList/PortfolioList.js
@@ -0,0 +1,11 @@
+function PortfolioList ({portfolioList}){
+
+    return(
+        <ul>
+            {
+                portfolioList.map((user,index)=> <li key ={index}>{user}</li>)
+            }
+        </ul>
+    )
+}
+export default PortfolioList;
